2b1f28949aeb3dac60d6b2af599b0ebfb200f182
kypdk@proton.me <kypdk@proton.me>
2023-12-25 00:46:02 +0300

    Apache BCEL'e geçildi

--- src/main/java/dev/padak/Agent.java
+++ src/main/java/dev/padak/Agent.java
@@ -1,114 +1,119 @@
 package dev.padak;
 
-import net.bytebuddy.agent.builder.AgentBuilder;
-import net.bytebuddy.asm.Advice;
-import net.bytebuddy.description.type.TypeDescription;
-import net.bytebuddy.dynamic.DynamicType;
-import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.matcher.ElementMatcher;
-import net.bytebuddy.matcher.ElementMatchers;
-import net.bytebuddy.utility.JavaModule;
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.lang.instrument.Instrumentation;
+import java.lang.instrument.ClassFileTransformer;
+import java.security.ProtectionDomain;
 
 public class Agent {
+    private static final Logger logger = LoggerFactory.getLogger(Agent.class);
 
-    private static final ElementMatcher<? super String> BYTE_BUDDY_LOGGING_FILTER;
+    public static void premain(String agentArgs, Instrumentation instrumentation) {
 
-    static {
-        String filterClass = System.getProperty("bytebuddy.debug.instrumentation.for.class");
-        BYTE_BUDDY_LOGGING_FILTER = filterClass != null ? s -> s.contains(filterClass) : s -> false;
+
+        instrumentation.addTransformer(new ExceptionLoggingTransformer());
     }
 
+    private static class ExceptionLoggingTransformer implements ClassFileTransformer {
+        @Override
+        public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
 
-    private static void mode4(Instrumentation instrumentation) {
+            String methodName = "";
 
-        AgentBuilder.Listener listener = new AgentBuilder.Listener() {
-            @Override
-            public void onDiscovery(String s, ClassLoader classLoader, JavaModule javaModule, boolean b) {
+            try {
 
+                if (className.startsWith("dev/padak/")) { // İlgili sınıfın adını değiştirin
+
+                    logger.info("Giriş:"+ className);
+
+                    JavaClass javaClass = new ClassParser(new ByteArrayInputStream(classfileBuffer), className + ".java").parse();
+                    ClassGen classGen = new ClassGen(javaClass);
+
+                    for (Method method : classGen.getMethods()) {
+
+                        methodName = method.getName();
+                        logger.info("Method adı: "+ method.getName());
+
+                        if (method.getCode() != null && !method.getName().equals("<init>")) {
+                            MethodGen methodGen = new MethodGen(method, className, classGen.getConstantPool());
+
+                            InstructionList instructionList = new InstructionList(methodGen.getMethod().getCode().getCode());
+
+                            // Exception dinleme kodu ve bilgileri alma
+                            addExceptionHandling(instructionList, methodGen, javaClass, className, method.getName());
+
+                            methodGen.setMaxStack();
+                            methodGen.setMaxLocals();
+                            classGen.replaceMethod(method, methodGen.getMethod());
+                        }
+                    }
+
+                    return classGen.getJavaClass().getBytes();
+                }
+
+            } catch (Exception e) {
+                logger.error("class: {} method: {} hata: {}", className, methodName, e);
+                return null;
             }
 
-            @Override
-            public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule, boolean b, DynamicType dynamicType) {
+            return null;
+        }
+    }
 
+
+    private static void addExceptionHandling(InstructionList instructionList, MethodGen methodGen, JavaClass javaClass, String className, String methodName) throws TargetLostException {
+
+        if (instructionList != null) {
+            // Check if there is a try-catch block
+            CodeExceptionGen[] exceptionHandlers = methodGen.getExceptionHandlers();
+
+            if (exceptionHandlers.length == 0) {
+                // If there is no try-catch block, add a new one
+                InstructionHandle tryStart = instructionList.getStart();
+                InstructionHandle tryEnd = instructionList.getEnd();
+
+
+                ObjectType exceptionType = new ObjectType("java.lang.Exception");
+
+                logger.info("Bu metod'ta tryCatch block yok {}",methodName);
+
+                InstructionList catchList = new InstructionList();
+
+
+                ConstantPoolGen constantPoolGen = methodGen.getConstantPool();  // ConstantPoolGen alınır
+                int sysOutFieldRef = constantPoolGen.addFieldref("java/lang/System", "out", "Ljava/io/PrintStream;");
+                int stringConstRef = constantPoolGen.addString("Exception caught");
+                int printlnMethodRef = constantPoolGen.addMethodref("java/io/PrintStream", "println", "(Ljava/lang/String;)V");
+
+                catchList.append(new GETSTATIC(sysOutFieldRef));
+                catchList.append(new LDC(stringConstRef));
+                catchList.append(new INVOKEVIRTUAL(printlnMethodRef));
+
+                InstructionHandle catchTarget = instructionList.append(catchList);
+
+                methodGen.addExceptionHandler(tryStart,tryEnd,catchTarget,exceptionType);
+
+
+                Method method = methodGen.getMethod();
+
+
+            } else {
+                logger.info("Bu metod'ta tryCatch block var {}",methodName);
+
+                logger.info("Exception sayisi {}",exceptionHandlers.length);
+
+                for(CodeExceptionGen tek: exceptionHandlers) {
+
+                    logger.info("Exception cesidi {}",tek.getCatchType().getClassName());
+
+                }
             }
-
-            @Override
-            public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule, boolean b) {
-
-            }
-
-            @Override
-            public void onError(String s, ClassLoader classLoader, JavaModule javaModule, boolean b, Throwable throwable) {
-
-            }
-
-            @Override
-            public void onComplete(String s, ClassLoader classLoader, JavaModule javaModule, boolean b) {
-                System.out.println("Completed: " + s);
-            }
-        };
-
-        new AgentBuilder.Default()
-                .type(ElementMatchers.any())
-                .transform((builder, type, classLoader, module) ->
-                        builder.method(ElementMatchers.any())
-                                .intercept(Advice.to(ExitInterceptor.class))
-                )
-                .with(listener)
-                .installOn(instrumentation);
+        }
     }
-
-    private static void mode5(Instrumentation instrumentation) {
-        new AgentBuilder.Default()
-                .with(AgentBuilder.Listener.StreamWriting.toSystemOut())
-                //.disableClassFormatChanges().with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
-                //.with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
-                .type(ElementMatchers.any())
-                .transform((builder, typeDescription, classLoader, module) -> builder
-                        .visit(Advice.to(ExitInterceptor.class).on(ElementMatchers.any()))
-                )
-                .installOn(instrumentation);
-    }
-    private static void mode1(Instrumentation instrumentation) {
-        new AgentBuilder.Default()
-                .type(ElementMatchers.any())
-                .transform((builder, type, classLoader, module) -> builder.method(
-                        ElementMatchers.any()).intercept(MethodDelegation.to(BodyInterceptor.class))
-                ).with(new AgentBuilder.Listener.Filtering(BYTE_BUDDY_LOGGING_FILTER, AgentBuilder.Listener.StreamWriting.toSystemOut()
-                )).installOn(instrumentation);
-    }
-
-    private static void mode3(Instrumentation instrumentation) {
-        new AgentBuilder.Default()
-                //.type(ElementMatchers.any())
-                .type(ElementMatchers.any())
-                .transform((builder, type, classLoader, module) ->
-                        builder.method(ElementMatchers.any()).intercept(MethodDelegation.to(BodyInterceptor.class))
-                ).installOn(instrumentation);
-    }
-
-    private static void mode2(Instrumentation instrumentation) {
-
-        new AgentBuilder.Default()
-                .disableClassFormatChanges()
-                .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
-                .with(AgentBuilder.Listener.StreamWriting.toSystemOut())
-                .type(ElementMatchers.any())
-                //.type(ElementMatchers.nameMatches(".*doProcess.*"))
-                //.type(ElementMatchers.hasSuperType(ElementMatchers.named("com.example.bytebuddylab.app.AbstractBusinessClass")))
-                .transform((builder, type, classLoader, module) ->
-                        builder.visit(Advice.to(TimerAdvice.class).on(ElementMatchers.any()))
-                ).installOn(instrumentation);
-
-    }
-
-    public static void premain(String arguments, Instrumentation instrumentation) {
-
-        System.out.println(" ******* Agent starts ******* ");
-        mode5(instrumentation);
-
-    }
-
 }
